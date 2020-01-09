26432e63a99c53bb49442e6f1cd8d661ad83aeb4
Pier Andrea Frontani <pierandreapaf@gmail.com>
2020-01-09 23:36:42 +0100

    Aggiungere i file di progetto.

---
+++ ExTepsit/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExTepsit
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object lblMedia;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcola_Click(object sender, RoutedEventArgs e)
+        {
+
+            double n1 = double.Parse(numero1.Text);
+            double n2 = double.Parse(numero2.Text);
+            double max;
+            double min;
+            if (n1 > n2)
+            {
+                max = n1;
+                min = n2;
+            }
+            else
+            {
+                max = n2;
+                min = n1;
+            }
+            double media = (n1 + n2) / 2;
+         
+        }
+    }
+}
---
+++ es1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("es1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("es1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a56d376a-fe9a-4ca9-9b45-35963cc9be78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
